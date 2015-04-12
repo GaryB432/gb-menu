8fb06105c50efdbf2fbc353eb9e334b039241bfd
Gary Bortosky <gary@bortosky.com>
2015-04-12 14:39:34 -0500

    move d.ts in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,10 @@ var gulp = require('gulp');
 var ts = require('gulp-typescript');
 var sourcemaps = require('gulp-sourcemaps');
 
+var gst = ['*.ts', '!*.d.ts']
+
 gulp.task('scripts', function () {
-    return tsResult = gulp.src('*.ts')
+    return tsResult = gulp.src(gst)
         .pipe(sourcemaps.init())
         .pipe(ts({ noImplicitAny: true })).js
         .pipe(sourcemaps.write('.'))
@@ -11,8 +13,9 @@ gulp.task('scripts', function () {
 });
 
 gulp.task('defs', function () {
-    return gulp.src('*.ts')
-        .pipe(ts({ declarationFiles: true, noImplicitAny: true })).dts.pipe(gulp.dest('typings/gbmenu'));
+    return gulp.src(gst)
+        .pipe(ts({ declarationFiles: true, noImplicitAny: true })).dts
+        .pipe(gulp.dest(''));
 });
 
 gulp.task('default', ['scripts', 'defs']);
